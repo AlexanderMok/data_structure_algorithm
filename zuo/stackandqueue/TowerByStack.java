13f6ee803cda372da21388e0bab553d80052ae80
moqinglai <qinglai.mo@shatacloud.com>
2017-04-20 11:30:04 +0800

    update towerByStack

--- zuo/stackandqueue/TowerByStack.java
+++ zuo/stackandqueue/TowerByStack.java
@@ -1,6 +1,8 @@
 package zuo.stackandqueue;
+
 /**
  * Tower problem
+ * 
  * <pre>
  * def tower(size, from, to, spare):
  *     if size==1:
@@ -8,36 +10,88 @@ package zuo.stackandqueue;
  *     else
  *         tower(size-1,from, spare, to)
  *         tower(1, from, to, spare)
- *         tower(size-1,spare, to, from)    
+ *         tower(size-1,spare, to, from)
  * </pre>
+ * 
+ * Now if mid is a must position when move from left to right or right to left.
  * Use stack to solve this problem
- * 汉诺威塔问题，修改规则：不可直接从最左移动到最右，反过来亦然，必须经过中间
- * 求有N层时，打印最优移动路径，和最优移动步数
+ * 
+ * 汉诺威塔问题，修改规则：不可直接从最左移动到最右，反过来亦然，必须经过中间 求有N层时，打印最优移动路径，和最优移动步数
+ * 
  * @author Alex
  *
  */
 public class TowerByStack {
-	
+
 	static int count = 0;
-	
-	public static void towerByStack(){
-		
-	}
-	
-	public static void tower(int size, String from, String to, String spare) {
+
+	/**
+	 * Traditional tower
+	 * 
+	 * @param size
+	 * @param from
+	 * @param to
+	 * @param spare
+	 */
+	public static void traditionalTower(int size, String from, String to, String spare) {
 		count++;
 		if (size == 1) {
 			System.out.printf("move disk from %s to %s. count %d \n", from, to, count);
 		} else {
-            tower(size - 1, from, spare, to);
-            tower(1, from, to, spare);
-            tower(size - 1, spare, to, from);
+			traditionalTower(size - 1, from, spare, to);
+			traditionalTower(1, from, to, spare);
+			traditionalTower(size - 1, spare, to, from);
 		}
 	}
-	
-    
+
+	static class TowerByStackRecursive {
+
+		public static int towerByStackRecursive(int size, String left, String mid, String right) {
+			if (size < 1) {
+				return 0;
+			}
+			return process(size, left, mid, right, left, right);
+		}
+
+		private static int process(int size, String left, String mid, String right, String from, String to) {
+			// 1 layer left and this is the base case
+			if (size == 1) {
+				/* left->mid right->mid mid->left mid->right */
+				if (from.equals(mid) || to.equals(mid)) {
+					System.out.printf("move 1 from %s to %s \n", from, to);
+					return 1;
+				} else {
+					/* every move has to pass through mid */
+					System.out.printf("move 1 from %s to %s \n", from, mid);
+					System.out.printf("move 1 from %s to %s \n", mid, to);
+					return 2;
+				}
+			}
+			// N-1 layer left
+			if (from.equals(mid) || to.equals(mid)) {
+				/*  */
+				String another = (from.equals(left) || to.equals(left)) ? right : left;
+				int part1 = process(size - 1, left, mid, right, from, another);
+				int part2 = 1;
+				System.out.printf("move %d from %s to %s \n", size, from, to);
+				int part3 = process(size, left, mid, right, another, to);
+				return part1 + part2 + part3;
+			} else {
+				int part1 = process(size - 1, left, mid, right, from, to);
+				int part2 = 1;
+				System.out.printf("move %d from %s to %s \n", size, from, mid);
+				int part3 = process(size - 1, left, mid, right, to, from);
+				int part4 = 1;
+				System.out.printf("move %d from %s to %s \n", size, mid, to);
+				int part5 = process(size - 1, left, mid, right, from, to);
+				return part1 + part2 + part3 + part4 + part5;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		tower(2, "left", "mid", "right");
+		// TowerByStack.traditionalTower(2, "left", "mid", "right");
+		TowerByStack.TowerByStackRecursive.towerByStackRecursive(2, "left", "mid", "right");
 	}
 
 }
