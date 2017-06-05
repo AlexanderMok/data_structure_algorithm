d82a55c84116451a71f0e5a7a2d553b4443bd6d4
moqinglai <qinglai.mo@shatacloud.com>
2017-06-05 12:36:35 +0800

    add stack implementation to Tower Problem

--- zuo/stackandqueue/TowerByStack.java
+++ zuo/stackandqueue/TowerByStack.java
@@ -15,8 +15,8 @@ import java.util.Stack;
  *         tower(size-1,spare, to, from)
  * </pre>
  * 
- * Now if mid is a must position when move from left to right or right to left.
- * Use stack to solve this problem
+ * Now if mid is a must-through position when move from left to right or right to left.
+ * Hint:Use stack to solve this problem
  * 
  * 汉诺威塔问题，修改规则：不可直接从最左移动到最右，反过来亦然，必须经过中间 求有N层时，打印最优移动路径，和最优移动步数
  * 
@@ -73,25 +73,43 @@ public class TowerByStack {
 			if (from.equals(mid) || to.equals(mid)) {
 				/*  */
 				String another = (from.equals(left) || to.equals(left)) ? right : left;
+				/* move size - 1 from 'from' to right*/
 				int part1 = process(size - 1, left, mid, right, from, another);
 				int part2 = 1;
+				/* move 1 from 'from' to 'mid'*/
 				System.out.printf("move %d from %s to %s \n", size, from, to);
+				/* move size from 'right' to 'mid'*/
 				int part3 = process(size, left, mid, right, another, to);
 				return part1 + part2 + part3;
 			} else {
+				/* move size - 1 from 'from' to 'right'*/
 				int part1 = process(size - 1, left, mid, right, from, to);
 				int part2 = 1;
+				/* move size from 'from' to 'mid'*/
 				System.out.printf("move %d from %s to %s \n", size, from, mid);
+				/* move size - 1 from 'right' to 'left'*/
 				int part3 = process(size - 1, left, mid, right, to, from);
 				int part4 = 1;
+				/* move size from 'mid' to 'right'*/
 				System.out.printf("move %d from %s to %s \n", size, mid, to);
+				/* move size - 1 from 'left' to 'right'*/
 				int part5 = process(size - 1, left, mid, right, from, to);
 				return part1 + part2 + part3 + part4 + part5;
 			}
 		}
 	}
 	
-	
+	/**
+	 * Stack to simulate the process<p>
+	 * 1.Only four movements: left to mid, mid to left, mid to right, right to mid<p>
+	 * 2.Three stacks to simulate left,mid and right<p>
+	 * 3.Two can be deduced <p>
+	 *   -> a.smaller elements can not push upon bigger elements in a stack b.adjacent movement can not be reversed<p>
+	 *   -> then it can be concluded that a.1st move has to be Left -> Mid, 
+	 *   b.To achieve the least movement, there is only one choice for each move among the 4 possible movement
+	 * @author Alex
+	 *
+	 */
 	static class TowerByStackNonRecur {
 		public enum Action {
 			/*still, left to mid, mid to left, mid to right, right to mid*/
@@ -102,15 +120,46 @@ public class TowerByStack {
 			Stack<Integer> lStack = new Stack<>();
 			Stack<Integer> mStack = new Stack<>();
 			Stack<Integer> rStack = new Stack<>();
-			return 0;
+			lStack.push(Integer.MAX_VALUE);
+			mStack.push(Integer.MAX_VALUE);
+			rStack.push(Integer.MAX_VALUE);
+			
+			/*init left stack*/
+			for(int i = size; i>0;i--) {
+				lStack.push(i);
+			}
+			Action[] record = {Action.No};
+			int step = 0;
+			
+			/*begin*/
+			while(rStack.size() != size + 1){
+				step += fromStackToStack(record, Action.M2L, Action.L2M, lStack, mStack, left, mid);
+				step += fromStackToStack(record, Action.L2M, Action.M2L, mStack, lStack, mid, left);
+				step += fromStackToStack(record, Action.R2M, Action.M2R, mStack, rStack, mid, right);
+				step += fromStackToStack(record, Action.M2R, Action.R2M, rStack, mStack, right, mid);
+			}
+			
+			return step;
 		}
-		
-		
+
+		private static int fromStackToStack(Action[] record, Action preNoAct, Action nowAct, Stack<Integer> fromStack,
+				Stack<Integer> toStack, String from, String to) {
+			/*1.smaller elements can not push upon bigger elements in a stack 2.adjacent movement can not be reversed*/
+			if (record[0] != preNoAct && fromStack.peek() < toStack.peek()) {
+				toStack.push(fromStack.pop());
+				System.out.printf("Move %d from %s to %s.\n", toStack.peek(), from, to);
+				record[0] = nowAct;
+			}
+			return 1;
+		}
 	}
 
 	public static void main(String[] args) {
-		// TowerByStack.traditionalTower(2, "left", "mid", "right");
-		TowerByStack.TowerByStackRecursive.towerByStackRecursive(2, "left", "mid", "right");
+		//TowerByStack.traditionalTower(2, "left", "mid", "right");
+		int step = TowerByStack.TowerByStackRecursive.towerByStackRecursive(2, "left", "mid", "right");
+		System.out.println("Recursive -> " + step);
+		step = TowerByStack.TowerByStackNonRecur.towerByStack(2, "left", "mid", "right");
+		System.out.println("Stack -> " + step);
 	}
 
 }
