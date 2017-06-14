029faf9da5faf109734b8c898372bdf334c1cb26
moqinglai <qinglai.mo@shatacloud.com>
2017-06-14 12:06:43 +0800

    add PrintCommonpart of two sorted list

---
+++ zuo/list/PrintCommonPart.java
@@ -0,0 +1,65 @@
+package zuo.list;
+
+/**
+ * Define a list
+ * @author Alex
+ *
+ */
+class Node {
+	public int value;
+	public Node next;
+	public Node(int data) {
+		this.value = data;
+	}
+	
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
+
+/**
+ * Print common part of two sorted list, given two heads of the list
+ * 打印两个有序链表的公共部分
+ * @author Alex
+ *
+ */
+public class PrintCommonPart {
+	
+	public static void printCommonPart(Node head1, Node head2){
+		System.out.println("Common part -> ");
+		while(head1 != null && head2 != null){
+			if (head1.value < head2.value) {
+				head1 = head1.next;
+			} else if(head1.value > head2.value) {
+                head2 = head2.next;
+			} else {
+				System.out.print(head1.value + " ");
+				head1 = head1.next;
+				head2 = head2.next;
+			}
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+        Node node1 = new Node(2);
+        Node node2 = new Node(4);
+        node1.setNext(node2);
+        Node node3 = new Node(6);
+        node2.setNext(node3);
+        Node node4 = new Node(8);
+        node3.setNext(node4);
+        
+        Node node5 = new Node(1);
+        Node node6 = new Node(3);
+        node5.setNext(node6);
+        Node node7 = new Node(4);
+        node6.setNext(node7);
+        Node node8 = new Node(8);
+        node7.setNext(node8);
+        Node node9 = new Node(9);
+        node8.setNext(node9);
+        
+        PrintCommonPart.printCommonPart(node1, node5);
+	}
+}
---
+++ zuo/stackandqueue/MaxTree.java
@@ -0,0 +1,39 @@
+package zuo.stackandqueue;
+
+
+/**
+ * Define a Binary Tree
+ * @author Alex
+ *
+ */
+class Node {
+	public int value;
+	public Node left;
+	public Node right;
+	public Node(int data) {
+		this.value = data;
+	}
+}
+
+
+
+/**
+ * Problem Set: Construct a MaxTree of an array<p>
+ * MaxTree Definition:<p>
+ * 1.No duplicated elements in an array<p>
+ * 2.MaxTree is a Binary Tree, each value in the array represents a node in the tree<p>
+ * 3.node with the largest value must be a head in the MaxTree and its sub tree<p>
+ * 
+ * Input -> an array with no duplication<p>
+ * Output -> MaxTree<p>
+ * 
+ * 1.Every parent node
+ * @author Alex
+ *
+ */
+public class MaxTree {
+    public static void main(String[] args) {
+    	int[] array = {9,6,5,33,74,44,3,13,2};
+    	
+	}
+}
