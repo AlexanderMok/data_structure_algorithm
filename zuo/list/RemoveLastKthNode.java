9eb93313ddb8565c3376cc462e8d3606d919a819
moqinglai <qinglai.mo@shatacloud.com>
2017-06-20 11:30:31 +0800

    add double list of removeLastKthNode

--- zuo/list/RemoveLastKthNode.java
+++ zuo/list/RemoveLastKthNode.java
@@ -15,8 +15,29 @@ public class RemoveLastKthNode {
         
         Node node = removeLastKthNodeSingleList(node1, 3);
         System.out.println(node);
+        
+        DoubleNode dn1 = new DoubleNode(2);
+        dn1.setLast(null);
+        DoubleNode dn2 = new DoubleNode(4);
+        dn1.setNext(dn2);
+        dn2.setLast(dn1);
+        DoubleNode dn3 = new DoubleNode(6);
+        dn2.setNext(dn3);
+        dn3.setLast(dn2);
+        DoubleNode dn4 = new DoubleNode(8);
+        dn3.setNext(dn4);
+        dn4.setLast(dn3);
+        dn4.setNext(null);
+        DoubleNode doubleNode = removeLastkthNodeDoubleList(dn1, 3);
 	}
 	
+	/**
+	 * 让链表从头开始早到尾，每移动一步，让lastkth值减1
+	 * 这样可以起到类似双指针的效果
+	 * @param singleList
+	 * @param lastKth
+	 * @return
+	 */
 	public static Node removeLastKthNodeSingleList(Node singleList, int lastKth){
 		if(singleList == null || lastKth < 1){
 			return singleList;
@@ -45,6 +66,35 @@ public class RemoveLastKthNode {
 		}
 		//lastkth > 0 means lastKth does not exist
 		return singleList;
+	}
+	
+	public static DoubleNode removeLastkthNodeDoubleList(DoubleNode doubleList, int lastKth) {
+		if(doubleList == null || lastKth < 1){
+			return doubleList;
+		}
+		DoubleNode cur = doubleList;
+		while(cur!=null) {
+			lastKth--;
+			cur = cur.next;
+		}
+		if(lastKth == 0){
+			doubleList = doubleList.next;
+			doubleList.last = null;
+		}
+		if (lastKth < 0 ) {
+			cur = doubleList;
+			while(++lastKth != 0){
+				cur = cur.next;
+			}
+			//Deletion of a double list
+			DoubleNode newNext = cur.next.next;
+			cur.next = newNext;
+			
+			if(newNext != null){
+				newNext.last = cur;
+			}
+		}
+		return doubleList;
 	}
 
 }
