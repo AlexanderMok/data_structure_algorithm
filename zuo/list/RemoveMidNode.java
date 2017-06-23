90bda7465b2325eb8c7315c4c70cf430e830f898
moqinglai <qinglai.mo@shatacloud.com>
2017-06-23 11:29:04 +0800

    add reverse single list

--- zuo/list/RemoveMidNode.java
+++ zuo/list/RemoveMidNode.java
@@ -17,43 +17,83 @@ public class RemoveMidNode {
 	 * @return
 	 */
 	public static Node removeMid(Node head) {
+		//链表长度为0或1
 		if (head == null || head.next == null) {
 			return head;
 		}
+		//链表长度为2
 		if (head.next.next == null) {
 			return head.next;
 		}
+		//链表长度大于2
 		Node pre = head;
 		Node cur = head.next.next;
+		//链表长度每增加2，要删除的节点后移1位
 		while (cur.next != null && cur.next.next != null) {
 			pre = pre.next;
 			cur = cur.next.next;
 		}
+		//deletion
 		pre.next = pre.next.next;
 		return head;
 	}
 	
 	/**
-	 * 
+	 * 1->2->3->4->5 a/b的值为r
+	 * r=0 不删除
+	 * r属于(0,1/5] 删除1
+	 * r属于(1/5,2/5] 删除2
+	 * r属于(2/5,3/5] 删除3
+	 * ..
+	 * r > 1 不删除
 	 * @param head
 	 * @param a
 	 * @param b
 	 * @return
 	 */
-	public static Node removeMidParam(Node head, int a, int b) {
-		return null;
+	public static Node removeMidByParam(Node head, int a, int b) {
+		if (a < 1 || a > b) {
+			return head;
+		}
+		//calc the length of the list
+		int n = length(head);
+		//this is the key
+		n = (int) Math.ceil(a * n / b);
+		if (n == 1) {
+			head = head.next;
+		}
+		if (n > 1) {
+			Node cur = head;
+			while(--n != 1){
+				cur = cur.next;
+			}
+			cur.next = cur.next.next;
+		}
+		return head;
+	}
+	
+	public static int length(Node head) {
+		int n = 0;
+		Node cur = head;
+		while (cur != null) {
+			n++;
+			cur = cur.next;
+		}
+		return n;
 	}
 
 	public static void main(String[] args) {
-		Node node1 = new Node(2);
+		Node head = new Node(2);
         Node node2 = new Node(4);
-        node1.setNext(node2);
+        head.setNext(node2);
         Node node3 = new Node(6);
         node2.setNext(node3);
         Node node4 = new Node(8);
         node3.setNext(node4);
+        Node node5 = new Node(9);
+        node4.setNext(node5);
         
-        Node remove = removeMid(node1);
+        Node remove = removeMid(head);
         System.out.println(remove);
 	}
 }
---
+++ zuo/list/ReverseList.java
@@ -0,0 +1,44 @@
+package zuo.list;
+
+public class ReverseList {
+    
+	/**
+	 * Method 1: In place reverse
+	 * two pointers
+	 * 两个指针
+	 * 1->2->3->4->5
+	 * @param head
+	 * @return
+	 */
+	public static Node reverseSingleList0(Node head){
+		Node pre = null;
+		//指向需要反转的节点
+		Node next = null;
+		while(head != null){
+			next = head.next;
+			//将原来指向下一节点的指针，指向辅助指针，断开其与连接的节点
+			//辅助指针记录着每次断开的节点
+			head.next = pre;
+			//更新结果
+			pre = head;
+			//更新头节点，头节点向后移动1位
+			head = next;
+		}
+		return pre;
+	}
+	
+	public static void main(String[] args) {
+		Node head = new Node(2);
+        Node node2 = new Node(4);
+        head.setNext(node2);
+        Node node3 = new Node(6);
+        node2.setNext(node3);
+        Node node4 = new Node(8);
+        node3.setNext(node4);
+        Node node5 = new Node(9);
+        node4.setNext(node5);
+        Node reversedList = reverseSingleList0(head);
+        System.out.println(reversedList);
+	}
+
+}
