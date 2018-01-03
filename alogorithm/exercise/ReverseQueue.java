d02d6cfd3451aeae8fc7882aca2cfbd8f3455ef1
moqinglai <qinglai.mo@shatacloud.com>
2018-01-03 10:42:11 +0800

    update ReverseQueue by block rearrangement

--- alogorithm/exercise/ReverseQueue.java
+++ alogorithm/exercise/ReverseQueue.java
@@ -2,23 +2,33 @@ package algorithm.exercise;
 
 import algorithm.structure.queue.Queue;
 import algorithm.structure.stack.Stack;
-
+/**
+ * Use a Stack to reverse a Queue
+ * @author Alex
+ *
+ */
 public class ReverseQueue {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
     	Queue<Integer> queue = new Queue<>();
     	queue.enqueue(1);
     	queue.enqueue(2);
     	queue.enqueue(3);
     	queue.enqueue(4);
     	queue.enqueue(5);
-    	System.out.println(queue.toString());
-    	Stack<Integer> stack = new Stack<>();
-    	while (!queue.isEmpty()) {
-            stack.push(queue.dequeue());			
+    	System.out.println(queue);
+    	reverseQueue(queue);
+    	System.out.println(queue);
+	}
+    
+    public static <T> Queue<T> reverseQueue(Queue<T> queue) {
+    	Queue<T> re = queue;
+    	Stack<T> stack = new Stack<>();
+    	while (!re.isEmpty()) {
+            stack.push(re.dequeue());			
 		}
     	while(!stack.isEmpty()) {
-    		queue.enqueue(stack.pop());
+    		re.enqueue(stack.pop());
     	}
-    	System.out.println(queue.toString());
-	}
+    	return re;
+    }
 }
