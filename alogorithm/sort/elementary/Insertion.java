1cb89f7178bb462f6a10e00c6c6d6079dca184ce
moqinglai <qinglai.mo@shatacloud.com>
2018-08-24 11:03:04 +0800

    1.add Insertion 2.move Selection to package sort.elementary

--- alogorithm/sort/elementary/Insertion.java
+++ alogorithm/sort/elementary/Insertion.java
@@ -1,32 +1,19 @@
-package alogorithm.sort;
+package alogorithm.sort.elementary;
 
-import java.util.Arrays;
 import java.util.Comparator;
 
-public class Selection {
-	private Selection() {
-	}
-	
-	public static void main(String[] args) {
-		Integer[] a = new Integer[] {3,5,7,23,9,5,67};
-		Selection.sort(a);
-		Arrays.stream(a).forEach(e -> System.out.print(e + " "));
-		System.out.println(Selection.isSorted(a));
-	}
+
+public class Insertion {
 
 	public static void sort(Comparable[] a) {
 		int n = a.length;
 		for (int i = 0; i < n; i++) {
-			int min = i;
-			for (int j = i + 1; j < n; j++) {
-				if (less(a[j], a[min])) {
-					min = j;
-				}
+			for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+				exchange(a, j, j - 1);
 			}
-			exchange(a, i, min);
 		}
 	}
-
+	
 	private static boolean less(Comparable v, Comparable w) {
 		return v.compareTo(w) < 0;
 	}
