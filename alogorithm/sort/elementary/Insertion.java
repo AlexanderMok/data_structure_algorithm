780223bb67dc23b029e423c0d02c2b96aa7f3c0d
moqinglai <qinglai.mo@shatacloud.com>
2018-09-13 12:50:57 +0800

    update Insertion

--- alogorithm/sort/elementary/Insertion.java
+++ alogorithm/sort/elementary/Insertion.java
@@ -37,4 +37,17 @@ public class Insertion extends Sort {
 			a[j] = v;
 		}
 	}
+
+	public static void sort3(Comparable[] a) {
+		int n = a.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j > 0; j--) {
+				if (less(a[j], a[j - 1])) {
+					exchange(a, j, j - 1);
+				} else {
+					break;
+				}
+			}
+		}
+	}
 }
