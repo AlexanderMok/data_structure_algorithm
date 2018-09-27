9bc15185e9049a4f46eeb73ca583dc1aa1cb280e
moqinglai <qinglai.mo@shatacloud.com>
2018-09-27 16:39:35 +0800

    update BinaryInsertion

--- alogorithm/sort/elementary/BinaryInsertion.java
+++ alogorithm/sort/elementary/BinaryInsertion.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 
 import algorithm.sort.Sort;
 
+/**
+ * apply binary search to find the smallest position to insert with regard the
+ * to-be-inserted element
+ * 
+ * @author Alex
+ *
+ */
 public class BinaryInsertion extends Sort {
 	private BinaryInsertion() {
 	}
@@ -21,8 +28,8 @@ public class BinaryInsertion extends Sort {
 			Comparable v = a[i];
 			int lo = 0;
 			int hi = i;
-            // binary search to find index j at which to insert a[i]
-            // ACHHI(M)NNPAX K BINARU, find to lower bound
+			// binary search to find index j at which to insert a[i]
+			// find to lower bound position to insert
 			while (lo < hi) {
 				int mid = lo + (hi - lo) / 2;
 				if (less(v, a[mid])) {
@@ -32,11 +39,11 @@ public class BinaryInsertion extends Sort {
 				}
 			}
             
-			//shift a[j],...a[i-1] to right
+			// shift a[j],...a[i-1] to right
 			for (int j = i; j > lo; --j) {
 				a[j] = a[j - 1];
 			}
-			//insert a[i] at index j, the lower bound
+			// insert a[i] at index j, the lower bound
 			a[lo] = v;
 		}
 	}
