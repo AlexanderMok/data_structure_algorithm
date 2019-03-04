72413f526043a8725c82112c9e3fe97505e0fdc1
moqinglai <qinglai.mo@shatacloud.com>
2019-03-04 16:48:07 +0800

    fixed SequentialSearchSymbolTable deletion method

--- src/algorithm/structure/table/SequentialSearchSymbolTable.java
+++ src/algorithm/structure/table/SequentialSearchSymbolTable.java
@@ -7,9 +7,7 @@ import java.util.NoSuchElementException;
  * <p>
  * This implementation uses a singly-linked list and sequential search.
  * 
- * put,delete O(n)
- * get,contains O(n) worst
- * size,isEmpty O(1)
+ * put,delete O(n) get,contains O(n) worst size,isEmpty O(1)
  * 
  * @author Alex
  *
@@ -17,19 +15,19 @@ import java.util.NoSuchElementException;
  * @param <V>
  */
 public class SequentialSearchSymbolTable<K, V> {
-    //number of key-value pairs
+	// number of key-value pairs
 	private int n;
-	//the linked-list of key-value pairs
+	// the linked-list of key-value pairs
 	private Node first;
-	
-	public SequentialSearchSymbolTable(){
-		
+
+	public SequentialSearchSymbolTable() {
+
 	}
-	
+
 	public int size() {
 		return n;
 	}
-	
+
 	public V get(K key) {
 		if (key == null) {
 			throw new IllegalArgumentException("key cannot be empty");
@@ -39,6 +37,7 @@ public class SequentialSearchSymbolTable<K, V> {
 		}
 		Node traverse = first;
 		while (traverse != null) {
+			System.out.print(traverse.value + " ");
 			if (key.equals(traverse.key)) {
 				return traverse.value;
 			}
@@ -46,15 +45,16 @@ public class SequentialSearchSymbolTable<K, V> {
 		}
 		return null;
 	}
-	
+
 	public void put(K key, V value) {
 		if (key == null) {
 			throw new IllegalArgumentException("key cannot be empty");
 		}
-		if(value == null) {
+		if (value == null) {
 			delete(key);
 			return;
 		}
+		// key in table
 		Node traverse = first;
 		while (traverse != null) {
 			if (key.equals(traverse.key)) {
@@ -63,11 +63,12 @@ public class SequentialSearchSymbolTable<K, V> {
 			}
 			traverse = traverse.next;
 		}
+		// key not in table
 		Node newFirst = new Node(key, value, first);
 		first = newFirst;
 		n++;
 	}
-	
+
 	public V delete(K key) {
 		if (key == null) {
 			throw new IllegalArgumentException("key cannot be empty");
@@ -75,61 +76,76 @@ public class SequentialSearchSymbolTable<K, V> {
 		if (isEmpty()) {
 			throw new NoSuchElementException("Symbol table is empty");
 		}
-		Node sentinal = new Node();
-		sentinal.next = first;
-		while(first != null) {
-			if (key.equals((first.key))) {
-				V v = first.value;
-				n--;
-				sentinal.next = first.next;
-				first = first.next;
-				sentinal = null;
-				return v;
-			}
+
+		if (first != null && key.equals(first.key)) {
 			first = first.next;
-			sentinal = sentinal.next;
+			return first.value;
 		}
-		sentinal = null;
+
+		Node p = first;
+		for (Node x = first.next; x != null; x = x.next) {
+			if (key.equals(x.key)) {
+				n--;
+				p.next = x.next;
+				return x.value;
+			}
+		}
 		return null;
 	}
-	
+
+	public V delte(K key) {
+		first = delete(first, key);
+		return first.value;
+	}
+
+	private Node delete(Node x, K key) {
+		if (x == null) {
+			return null;
+		}
+		if (key.equals(x.key)) {
+			n--;
+			return x.next;
+		}
+		x.next = delete(x.next, key);
+		return x;
+	}
+
 	public boolean contains(K key) {
 		if (key == null) {
 			throw new IllegalArgumentException("key cannot be empty");
 		}
 		return get(key) != null;
 	}
-	
+
 	public boolean isEmpty() {
 		return size() == 0;
 	}
-	
-	
-	
-	
+
 	private class Node {
-    	private K key;
-    	private V value;
-    	private Node next;
-    	
-    	public Node(K key, V value, Node next) {
-    		this.key = key;
-    		this.value = value;
-    		this.next = next;
-    	}
-    	
-    	public Node() {
-    		
-    	}
-    }
-	
+		private K key;
+		private V value;
+		private Node next;
+
+		public Node(K key, V value, Node next) {
+			this.key = key;
+			this.value = value;
+			this.next = next;
+		}
+
+		public Node() {
+
+		}
+	}
+
 	public static void main(String[] args) {
 		SequentialSearchSymbolTable<String, Integer> st = new SequentialSearchSymbolTable<String, Integer>();
 		st.put("a", 1);
 		st.put("b", 2);
+		st.put("c", 3);
 		System.out.println(st.contains("a"));
 		System.out.println(st.get("b"));
-		System.out.println(st.delete("b"));
+		System.out.println(st.delte("b"));
 		System.out.println(st.get("b"));
+		System.out.println(st.get("c"));
 	}
 }
