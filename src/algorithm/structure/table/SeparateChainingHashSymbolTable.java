e67bb05cd71c56f0a3d3e700869b8e3d8edb4ecd
moqinglai <qinglai.mo@shatacloud.com>
2019-03-07 16:26:59 +0800

    add resize to SeparateChainingHashSymbolTable

--- src/algorithm/structure/table/SeparateChainingHashSymbolTable.java
+++ src/algorithm/structure/table/SeparateChainingHashSymbolTable.java
@@ -4,8 +4,17 @@ import java.util.stream.IntStream;
 
 import algorithm.structure.queue.Queue;
 
+/**
+ * Separate chaining to hashing collision resolution with
+ * SequentialSearchSymbolTable. Array chain with linked list implementation of  ST.
+ * 
+ * @author Alex
+ *
+ * @param <K>
+ * @param <V>
+ */
 public class SeparateChainingHashSymbolTable<K, V> {
-    private static final int INIT_CAPACITY = 4;
+	private static final int INIT_CAPACITY = 4;
 	// number of key-value pairs
 	private int n;
 	// hash table size
@@ -14,11 +23,12 @@ public class SeparateChainingHashSymbolTable<K, V> {
 	private SequentialSearchSymbolTable<K, V>[] st;
 
 	public SeparateChainingHashSymbolTable() {
-        this(INIT_CAPACITY);
+		this(INIT_CAPACITY);
 	}
-    
+
 	/**
 	 * Initialize an empty ST with {@code m} chains.
+	 * 
 	 * @param m
 	 */
 	public SeparateChainingHashSymbolTable(int m) {
@@ -28,45 +38,66 @@ public class SeparateChainingHashSymbolTable<K, V> {
 			st[i] = new SequentialSearchSymbolTable<K, V>();
 		});
 	}
-	
+
 	/**
-	 * hash the key between 0 and m-1.
-	 * change to 31-bit non-negative value and do modular hashing.
+	 * hash the key between 0 and m-1. change to 31-bit non-negative value and
+	 * do modular hashing.
+	 * 
 	 * @param key
 	 * @return
 	 */
 	private int hash(K key) {
 		return (key.hashCode() & 0x7fffffff) % m;
 	}
-	
+
 	private void checkKey(K key) {
 		if (key == null) {
 			throw new IllegalArgumentException();
 		}
 	}
-	
+
+	/**
+	 * resize the hash table but the size of chain remain unchanged.
+	 * 
+	 * @param chains
+	 */
+	private void resize(int chains) {
+		SeparateChainingHashSymbolTable<K, V> tmp = new SeparateChainingHashSymbolTable<>(chains);
+		for (int i = 0; i < m; i++) {
+			for (K key : st[i].keys()) {
+				tmp.put(key, st[i].get(key));
+			}
+		}
+		this.m = tmp.m;
+		this.n = tmp.n;
+		this.st = tmp.st;
+	}
+
 	public int size() {
 		return n;
 	}
-	
+
 	public boolean isEmpty() {
 		return size() == 0;
 	}
-	
+
 	public boolean contains(K key) {
 		checkKey(key);
 		return get(key) != null;
 	}
-	
+
 	public V get(K key) {
 		return st[hash(key)].get(key);
 	}
-	
+
 	public void put(K key, V value) {
 		checkKey(key);
-		if(value == null) {
+		if (value == null) {
 			delete(key);
 			return;
+		}
+		if (n >= 10 * m) {
+			resize(2 * m);
 		}
 		int i = hash(key);
 		if (!st[i].contains(key)) {
@@ -74,20 +105,23 @@ public class SeparateChainingHashSymbolTable<K, V> {
 		}
 		st[i].put(key, value);
 	}
-	
+
 	public void delete(K key) {
 		checkKey(key);
 		int i = hash(key);
 		if (st[i].contains(key)) {
 			n--;
 		}
-		st[i].delete(key);
+		st[i].delte(key);
+		if (n <= 2 * m && m > INIT_CAPACITY) {
+			resize(m / 2);
+		}
 	}
-	
+
 	public Iterable<K> keys() {
 		Queue<K> queue = new Queue<K>();
 		for (int i = 0; i < m; i++) {
-			for(K key : st[i].keys()){
+			for (K key : st[i].keys()) {
 				queue.enqueue(key);
 			}
 		}
@@ -95,5 +129,11 @@ public class SeparateChainingHashSymbolTable<K, V> {
 	}
 
 	public static void main(String[] args) {
+		SeparateChainingHashSymbolTable<String, String> map = new SeparateChainingHashSymbolTable<>();
+	    map.put("A", "1");
+	    map.put("B", "2");
+	    map.put("C", "3");
+	    map.put("D", "4");
+	    System.out.println(map.keys());
 	}
 }
