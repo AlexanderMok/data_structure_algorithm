4b4cf7f4f50a0af38d529674b70444277be63125
moqinglai <qinglai.mo@shatacloud.com>
2019-03-21 15:18:30 +0800

    udpate DepthFirstSearch

--- src/algorithm/structure/graph/DepthFirstSearch.java
+++ src/algorithm/structure/graph/DepthFirstSearch.java
@@ -16,6 +16,7 @@ public class DepthFirstSearch {
 	}
 
 	private void dfs(UndirectedGraph graph, int vertex) {
+		validateVertex(vertex);
 		marked[vertex] = true;
 		count++;
 		for (int w : graph.adjacent(vertex)) {
@@ -26,10 +27,36 @@ public class DepthFirstSearch {
 	}
 
 	public boolean isMarked(int vertex) {
+		validateVertex(vertex);
 		return marked[vertex];
 	}
 
 	public int count() {
 		return count;
+	}
+
+	private void validateVertex(int vertex) {
+		int v = marked.length;
+		if (vertex < 0 || vertex >= v) {
+			throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (v - 1));
+		}
+	}
+
+	public static void main(String[] args) {
+		UndirectedGraph graph = new UndirectedGraph(3);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 2);
+		graph.addEdge(1, 2);
+		DepthFirstSearch search = new DepthFirstSearch(graph, 2);
+		for (int v = 0; v < graph.vertices(); v++) {
+			if (search.isMarked(v)) {
+				System.out.print(v + " ");
+			}
+		}
+		if (search.count != graph.vertices()) {
+			System.out.println("NOT connected graph");
+		} else {
+			System.out.println("Connected graph");
+		}
 	}
 }
