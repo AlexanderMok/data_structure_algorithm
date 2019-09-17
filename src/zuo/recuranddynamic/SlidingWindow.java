0e38bbb8e298f0da1f5bc66ee4b43e0046f78c78
moqinglai <qinglai.mo@shatacloud.com>
2019-09-17 11:22:10 +0800

    update sliding window of lengthOfLongestSubstringSlidingWindow

--- src/zuo/recuranddynamic/SlidingWindow.java
+++ src/zuo/recuranddynamic/SlidingWindow.java
@@ -5,9 +5,11 @@ import java.util.Set;
 
 public class SlidingWindow {
 	public static void main(String[] args) {
-		String string = "";
+		String string = "abcabcbbvmokrhg";
 		LengthOfLongestSubstring instance = new LengthOfLongestSubstring();
 		int l = instance.lengthOfLongestSubstring(string);
+		int m = instance.lengthOfLongestSubstringSlidingWindow(string);
+		System.out.println(l + " : " + m);
 	}
 }
 
@@ -50,6 +52,17 @@ class LengthOfLongestSubstring {
 	 * 使用HashSet作为一个滑动窗口，检查一个字符是否已经存在于现有的子字符中。 滑动窗口经常作为一个抽象的概念来处理数组/字符串问题。
 	 * 窗口代表着一组数据/字符串元素，通过开头和结尾的索引来定义窗口。
 	 * 
+	 * Use Set as a sliding window to check whether a character exists in the
+	 * substring.
+	 * <p>
+	 * Sliding window is often used as an abstraction to solve array/string
+	 * problems.
+	 * <p>
+	 * Window represents a set of data/characters, using head and tail pointer
+	 * to define its range.
+	 * <P>
+	 * 
+	 * Worst O(2n). {@code i} and {@code j} both traverse to the end.
 	 * @param s
 	 * @return
 	 */
@@ -57,10 +70,21 @@ class LengthOfLongestSubstring {
 		int n = s.length();
 		Set<Character> window = new HashSet<>();
 		int length = 0, i = 0, j = 0;
-		// keep window [i, j] within range 
+		// keep window [i, j] within range
 		while (i < n && j < n) {
-			
-		} 
+			// expand upper bound 
+			if (!window.contains(s.charAt(j))) {
+				window.add(s.charAt(j));
+				j++;
+				// update length
+				length = Math.max(length, j - i);
+			} 
+			// expand lower bound
+			else {
+				window.remove(s.charAt(i));
+				i++;
+			}
+		}
 		return length;
 	}
 }
